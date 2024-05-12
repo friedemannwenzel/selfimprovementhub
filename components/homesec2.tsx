d3bf11ec343abec1fbd4762a3f3f2c2740a4dcd9
frodo270 <frodo@bewebe.de>
2024-05-12 17:52:58 +0200

    cosmetic changes on the home page

--- components/homesec2.tsx
+++ components/homesec2.tsx
@@ -9,8 +9,8 @@ const homesec2 = () => {
     <>
     <div className='bg-black text-white py-24 mt-44'>
       <h1 className='text-5xl font-bold m-auto max-w-screen-xl mb-8'>Explore your favorite creators</h1>
-      <div className='grid gap-6 lg:grid-cols-3 grid-rows-1 md:grid-cols-2 sm:grid-cols-1 m-auto max-w-screen-xl'>
-      {creator.map((creator) => (
+      <div className='grid gap-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 m-auto max-w-screen-xl mb-8'>
+      {creator.slice(0, 3).map((creator) => (
         <Link href={`creator/${creator.id}`} key={creator.id}>
         <div className='bg-emerald-100 rounded-xl min-h-[350px] hover:scale-105 transition-all text-black'>
           <Image src={creator.imageURL} alt={creator.altText} width={500} height={200} className='rounded-tl-xl rounded-tr-xl'/>
@@ -22,6 +22,11 @@ const homesec2 = () => {
         </div>
         </Link>
       ))}
+      </div>
+      <div className='flex justify-center items-center'>
+        <Link href={`/creator`}>
+          <Button color='secondary' variant='shadow' size='lg'>more</Button>
+        </Link>
       </div>
     </div>
     </>
