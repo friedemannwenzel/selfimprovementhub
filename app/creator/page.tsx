ad1452879751e1e16c5a97514fa0e0d12af028e9
frodo270 <frodo@bewebe.de>
2024-05-30 11:48:23 +0200

    margin changes

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -5,7 +5,7 @@ import ArticleCard from '@/components/ArticleCard';
 export const runtime ="edge";
 
 const creator = () => (
-  <div className=''>
+  <div className='sm:max-w-sm py-1'>
     <h1 className='text-5xl font-bold text-center my-28'>Discover your favorite creator</h1>
     <div className='grid gap-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-rows-1 m-auto max-w-screen-xl'>
       {articleData.slice().reverse().map((data, index) => (
